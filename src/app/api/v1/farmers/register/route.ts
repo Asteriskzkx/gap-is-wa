b4602aacf879db54e664336fb33a3208a5f1bb5f
Asteriskzkx <assassinaom@gmail.com>
2025-11-05 13:49:33 +0700

    fix: register page

--- src/app/api/v1/farmers/register/route.ts
+++ src/app/api/v1/farmers/register/route.ts
@@ -1,16 +1,6 @@
-import { checkAuthorization } from "@/lib/session";
 import { farmerController } from "@/utils/dependencyInjections";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { authorized, error } = await checkAuthorization(req, ["ADMIN"]);
-
-  if (!authorized) {
-    return NextResponse.json(
-      { message: error || "Unauthorized" },
-      { status: 401 }
-    );
-  }
-
   return farmerController.registerFarmer(req);
 }
