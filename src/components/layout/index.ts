2aa5f48991dc65ba4e68a82480705695dde5d396
Asteriskzkx <assassinaom@gmail.com>
2025-11-05 10:08:56 +0700

    fix: committee layout to new one

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,11 +2,7 @@
 export { default as HeaderNew } from "./HeaderNew";
 export { default as SidebarNew } from "./SidebarNew";
 export { default as FooterNew } from "./FooterNew";
-export { default as AuditorLayoutNew } from "./AuditorLayoutNew";
 
-// Legacy components (keep for backward compatibility)
-export { default as Header } from "./Header";
-export { default as Sidebar } from "./Sidebar";
-export { default as Footer } from "./Footer";
 export { default as AuditorLayout } from "./AuditorLayout";
 export { default as FarmerLayout } from "./FarmerLayout";
+export { default as CommitteeLayout } from "./CommitteeLayout";
