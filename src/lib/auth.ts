290ddbb0591b1af01ffc7bd1a4ffe98b67c734f7
Asteriskzkx <assassinaom@gmail.com>
2026-01-07 09:48:46 +0700

    fix: removeing unused model

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,9 @@
 import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { prisma } from "@/utils/db";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
